60dfaf771ba51c115258b8dcac88217a837a1c74
Gabriel Locoselli Oliveira <gabriel.locoselli@gmail.com>
2020-09-26 14:29:55 +0000

    Cleared Target

---
+++ rhsoft/src/test/java/br/com/gabriel/rhsoft/controllers/DepartmentsControllerTest.java
@@ -0,0 +1,23 @@
+package br.com.gabriel.rhsoft.controllers;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import br.com.gabriel.rhsoft.builders.CompanyBuilder;
+import br.com.gabriel.rhsoft.conf.AppWebConfiguration;
+import br.com.gabriel.rhsoft.conf.DataSourceConfigurationTest;
+import br.com.gabriel.rhsoft.conf.JPAConfiguration;
+import br.com.gabriel.rhsoft.testing_daos.CompanyDAOForTesting;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JPAConfiguration.class, AppWebConfiguration.class, DataSourceConfigurationTest.class, CompanyBuilder.class, CompanyDAOForTesting.class})
+@ActiveProfiles("test")
+public class DepartmentsControllerTest extends ControllerTest{
+
+    private CompanyBuilder companyBuilder = new CompanyBuilder();
+    
+}
